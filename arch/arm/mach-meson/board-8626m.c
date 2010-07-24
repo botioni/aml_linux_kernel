0dbe40ef47297e1329d78dae2810f91c9f1f6494
timyao <timyao@34fca4b9-5ec2-4a8b-9ba4-7e5d7900cddd>
2010-07-24 01:54:35 +0000

    Increase reserved OSD buffer size to 1080P with double buffer.
    git-svn-id: https://10.8.10.5/svn/Project_ARMLinux/trunk/kernel/v2.6.34@135 34fca4b9-5ec2-4a8b-9ba4-7e5d7900cddd

--- arch/arm/mach-meson/board-8626m.c
+++ arch/arm/mach-meson/board-8626m.c
@@ -26,11 +26,13 @@
 #include <mach/lm.h>
 #include <asm/memory.h>
 #include <asm/mach/map.h>
-#include "board-8626m.h"
 
 #ifdef CONFIG_CACHE_L2X0
 #include <asm/hardware/cache-l2x0.h>
 #endif
+
+#include "board-8626m.h"
+
 #if defined(CONFIG_JPEGLOGO)
 static struct resource jpeglogo_resources[] = {
     [0] = {
@@ -60,12 +62,20 @@ static struct resource fb_device_resources[] = {
         .end   = OSD1_ADDR_END,
         .flags = IORESOURCE_MEM,
     },
-    [1] ={ //for osd2
+    [1] = {
         .start = OSD2_ADDR_START,
         .end   =OSD2_ADDR_END,
         .flags = IORESOURCE_MEM,
     },
 };
+
+static struct platform_device fb_device = {
+    .name       = "apollofb",
+    .id         = 0,
+    .num_resources = ARRAY_SIZE(fb_device_resources),
+    .resource      = fb_device_resources,
+};
+#endif
 
 #ifdef CONFIG_USB_DWC_OTG_HCD
 struct lm_device usb_ld_b = {
@@ -81,13 +91,6 @@ struct lm_device usb_ld_b = {
 };
 #endif
 
-static struct platform_device fb_device = {
-    .name       = "apollofb",
-    .id         = 0,
-    .num_resources = ARRAY_SIZE(fb_device_resources),
-    .resource      = fb_device_resources,
-};
-#endif
 #if defined(CONFIG_AM_STREAMING)
 static struct resource apollo_codec_resources[] = {
     [0] = {
@@ -104,9 +107,11 @@ static struct platform_device apollo_codec = {
     .resource      = apollo_codec_resources,
 };
 #endif
+
+#if defined(CONFIG_AML_AUDIO_DSP)
 static struct resource apollo_audiodsp_resources[] = {
     [0] = {
-        .start =  AUDIODSP_ADDR_START,
+        .start = AUDIODSP_ADDR_START,
         .end   = AUDIODSP_ADDR_END,
         .flags = IORESOURCE_MEM,
     },
@@ -118,20 +123,7 @@ static struct platform_device apollo_audiodsp = {
     .num_resources = ARRAY_SIZE(apollo_audiodsp_resources),
     .resource      = apollo_audiodsp_resources,
 };
-static struct resource apollo_mali_resources[] = {
-    [0] = {
-        .start = MALI_ADDR_START,
-        .end   = MALI_ADDR_END,
-        .flags = IORESOURCE_MEM,
-    },
-};
-
-static struct platform_device apollo_mali= {
-    .name       = "mali400",
-    .id         = 0,
-    .num_resources = ARRAY_SIZE(apollo_mali_resources),
-    .resource      = apollo_mali_resources,
-};
+#endif
 
 
 static struct platform_device __initdata *platform_devs[] = {
@@ -142,18 +134,19 @@ static struct platform_device __initdata *platform_devs[] = {
     	&fb_device,
     #endif
     #if defined(CONFIG_AM_STREAMING)
-	&apollo_codec,
+		&apollo_codec,
     #endif
-	&apollo_audiodsp,
-	&apollo_mali,
+	#if defined(CONFIG_AML_AUDIO_DSP)
+		&apollo_audiodsp,
+	#endif
 };
 
 static __init void m1_init_machine(void)
 {
 #ifdef CONFIG_CACHE_L2X0
-		/* 128kb (16KB/way), 8-way associativity, evmon/parity/share disabled
-		 * Bits:  .... .... .000 0010 0000 .... .... .... */
-		l2x0_init((void __iomem *)IO_PL310_BASE, 0x00020000, 0xff800fff);
+	/* 128kb (16KB/way), 8-way associativity, evmon/parity/share disabled
+	 * Bits:  .... .... .000 0010 0000 .... .... .... */
+	l2x0_init((void __iomem *)IO_PL310_BASE, 0x00020000, 0xff800fff);
 #endif
 	platform_add_devices(platform_devs, ARRAY_SIZE(platform_devs));
 	/* todo: load device drivers */
@@ -162,14 +155,14 @@ static __init void m1_init_machine(void)
 	lm_device_register(&usb_ld_b);
 #endif
 }
+
 /*VIDEO MEMORY MAPING*/
 static __initdata struct map_desc meson_video_mem_desc[] = {
 	{
-		/*FIXME:map the video memory to other.*/
-		.virtual		=	PAGE_ALIGN(VIDEO_MEM_START),
-		.pfn			= 	__phys_to_pfn(VIDEO_MEM_START),
-		.length		= 	VIDEO_MEM_END-VIDEO_MEM_START+1,
-		.type		= 	MT_DEVICE,
+		.virtual	= PAGE_ALIGN(__phys_to_virt(RESERVED_MEM_START)),
+		.pfn		= __phys_to_pfn(RESERVED_MEM_START),
+		.length		= RESERVED_MEM_END-RESERVED_MEM_START+1,
+		.type		= MT_DEVICE,
 	},
 };
 
@@ -188,16 +181,16 @@ static __init void m1_irq_init(void)
 static __init void m1_fixup(struct machine_desc *mach, struct tag *tag, char **cmdline, struct meminfo *m)
 {
 	struct membank *pbank;
-	m->nr_banks=0;
+	m->nr_banks = 0;
 	pbank=&m->bank[m->nr_banks];
 	pbank->start = PAGE_ALIGN(PHYS_MEM_START);
 	pbank->size  = SZ_64M & PAGE_MASK;
 	pbank->node  = PHYS_TO_NID(PHYS_MEM_START);
 	m->nr_banks++;
 	pbank=&m->bank[m->nr_banks];
-	pbank->start = PAGE_ALIGN(VIDEO_MEM_END+1);
-	pbank->size  = (PHYS_MEM_END-VIDEO_MEM_END) & PAGE_MASK;
-	pbank->node  = PHYS_TO_NID(VIDEO_MEM_END+1);
+	pbank->start = PAGE_ALIGN(RESERVED_MEM_END+1);
+	pbank->size  = (PHYS_MEM_END-RESERVED_MEM_END) & PAGE_MASK;
+	pbank->node  = PHYS_TO_NID(RESERVED_MEM_END+1);
 	m->nr_banks++;
 }
 
@@ -209,7 +202,5 @@ MACHINE_START(MESON_8626M, "AMLOGIC MESON-M1 8626M")
 	.init_irq		= m1_irq_init,
 	.timer			= &meson_sys_timer,
 	.init_machine	= m1_init_machine,
-	.fixup		=m1_fixup,
-	.video_start	=VIDEO_MEM_START,
-	.video_end	=VIDEO_MEM_END,
+	.fixup			= m1_fixup,
 MACHINE_END
