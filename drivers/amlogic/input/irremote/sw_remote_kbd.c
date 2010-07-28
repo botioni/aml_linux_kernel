e7211ecf652fd0c14bcb49e1345050c4b4c1d4da
timyao <timyao@34fca4b9-5ec2-4a8b-9ba4-7e5d7900cddd>
2010-07-28 22:01:54 +0000

    Remove "Apollo" text from input driver.
    git-svn-id: https://10.8.9.8/svn/Project_ARMLinux/trunk/kernel/v2.6.34@164 34fca4b9-5ec2-4a8b-9ba4-7e5d7900cddd

--- drivers/amlogic/input/irremote/sw_remote_kbd.c
+++ drivers/amlogic/input/irremote/sw_remote_kbd.c
@@ -1,7 +1,7 @@
 /*
- * linux/drivers/input/irremote/apollo_remote_kbd.c
+ * linux/drivers/input/irremote/sw_remote_kbd.c
  *
- * apollo Keypad Driver
+ * Keypad Driver
  *
  * Copyright (C) 2009 Amlogic Corporation
  *
@@ -47,16 +47,16 @@ extern  char *remote_log_buf;
 
 static int  get_pulse_width(unsigned long data)
 {
-	struct apollo_kp *apollo_kp_data = (struct apollo_kp *) data;
+	struct kp *kp_data = (struct kp *) data;
 	int  pulse_width;
 	char buf[100];
 	const char* state;
 
 	pulse_width     = ( (READ_MPEG_REG( IR_DEC_REG1)) & 0x1FFF0000 ) >> 16 ;
-	state = apollo_kp_data->step==REMOTE_STATUS_WAIT?"wait":\
-				apollo_kp_data->step==REMOTE_STATUS_LEADER?"leader":\
-				apollo_kp_data->step==REMOTE_STATUS_DATA?"data":\
-				apollo_kp_data->step==REMOTE_STATUS_SYNC?"sync":NULL;
+	state = kp_data->step==REMOTE_STATUS_WAIT?"wait":\
+				kp_data->step==REMOTE_STATUS_LEADER?"leader":\
+				kp_data->step==REMOTE_STATUS_DATA?"data":\
+				kp_data->step==REMOTE_STATUS_SYNC?"sync":NULL;
 				
 	sprintf(buf,"%d:pulse_wdith:%d==>%s\r\n",pulse_index++,pulse_width,state);
 	if(strlen(remote_log_buf)>REMOTE_LOG_BUF_LEN)
@@ -67,13 +67,13 @@ static int  get_pulse_width(unsigned long data)
 	//sometimes we found remote  pulse width==0.	in order to sync machine state we modify it .
 	if(pulse_width==0)
 	{
-		switch(apollo_kp_data->step)
+		switch(kp_data->step)
 		{
 			case REMOTE_STATUS_LEADER:
-			pulse_width=apollo_kp_data->time_window[0] +1;	
+			pulse_width=kp_data->time_window[0] +1;	
 			break;
 			case REMOTE_STATUS_DATA:
-			pulse_width= apollo_kp_data->time_window[2] +1 ;	
+			pulse_width= kp_data->time_window[2] +1 ;	
 			break;	
 		}
 	}
@@ -83,55 +83,55 @@ static int  get_pulse_width(unsigned long data)
 static inline void kbd_software_mode_remote_wait(unsigned long data)
 {
 	unsigned short pulse_width;
-	struct apollo_kp *apollo_kp_data = (struct apollo_kp *) data;
+	struct kp *kp_data = (struct kp *) data;
 
 	pulse_width     = get_pulse_width(data) ;
-	apollo_kp_data->step   = REMOTE_STATUS_LEADER;
-	apollo_kp_data->cur_keycode = 0 ;
-	apollo_kp_data->bit_num = apollo_kp_data->bit_count ;
+	kp_data->step   = REMOTE_STATUS_LEADER;
+	kp_data->cur_keycode = 0 ;
+	kp_data->bit_num = kp_data->bit_count ;
 }
 static inline void kbd_software_mode_remote_leader(unsigned long data)
 {
 	unsigned short pulse_width;
-	struct apollo_kp *apollo_kp_data = (struct apollo_kp *) data;
+	struct kp *kp_data = (struct kp *) data;
 
 	pulse_index=0;
    	pulse_width     = get_pulse_width(data) ;
-	if((pulse_width > apollo_kp_data->time_window[0]) && (pulse_width <apollo_kp_data->time_window[1])) {
-	    	apollo_kp_data->step   = REMOTE_STATUS_DATA;
+	if((pulse_width > kp_data->time_window[0]) && (pulse_width <kp_data->time_window[1])) {
+	    	kp_data->step   = REMOTE_STATUS_DATA;
 	}
     	else {
-      		apollo_kp_data->step    = REMOTE_STATUS_WAIT ;
+      		kp_data->step    = REMOTE_STATUS_WAIT ;
 	}
 
 	//RemoteStatus   = REMOTE_STATUS_DATA;
-	apollo_kp_data->cur_keycode = 0 ;
-	apollo_kp_data->bit_num = apollo_kp_data->bit_count ;
+	kp_data->cur_keycode = 0 ;
+	kp_data->bit_num = kp_data->bit_count ;
 }
 static inline void kbd_software_mode_remote_send_key(unsigned long data)
 {
-  	struct apollo_kp *apollo_kp_data = (struct apollo_kp *) data;
-	unsigned int  reort_key_code=apollo_kp_data->cur_keycode>>16&0xffff;
+  	struct kp *kp_data = (struct kp *) data;
+	unsigned int  reort_key_code=kp_data->cur_keycode>>16&0xffff;
 	
-	apollo_kp_data->step   = REMOTE_STATUS_SYNC ;
-    	if(apollo_kp_data->repeate_flag)
+	kp_data->step   = REMOTE_STATUS_SYNC ;
+    	if(kp_data->repeate_flag)
     	{
-		if(apollo_kp_data->custom_code != (apollo_kp_data->cur_keycode&0xffff ))
+		if(kp_data->custom_code != (kp_data->cur_keycode&0xffff ))
 		{
 			return ;
 		}
 		if(((reort_key_code&0xff)^(reort_key_code>>8&0xff))==0xff)
-		input_report_key(apollo_kp_data->input, (apollo_kp_data->cur_keycode>>16)&0xff, 2);
-		input_dbg("key repeate,scan code :0x%x\r\n", apollo_kp_data->cur_keycode);
+		input_report_key(kp_data->input, (kp_data->cur_keycode>>16)&0xff, 2);
+		input_dbg("key repeate,scan code :0x%x\r\n", kp_data->cur_keycode);
     	}else{
-		if(apollo_kp_data->custom_code != (apollo_kp_data->cur_keycode&0xffff ))
+		if(kp_data->custom_code != (kp_data->cur_keycode&0xffff ))
 		{
-		       input_dbg("Wrong custom code is 0x%04x\n", apollo_kp_data->cur_keycode&0xffff);
+		       input_dbg("Wrong custom code is 0x%04x\n", kp_data->cur_keycode&0xffff);
 			return ;
 		}
 		if(((reort_key_code&0xff)^(reort_key_code>>8&0xff))==0xff)
-		input_report_key(apollo_kp_data->input, (apollo_kp_data->cur_keycode>>16)&0xff, 1);	
-		input_dbg("key pressed,scan code :0x%x\r\n", apollo_kp_data->cur_keycode);
+		input_report_key(kp_data->input, (kp_data->cur_keycode>>16)&0xff, 1);	
+		input_dbg("key pressed,scan code :0x%x\r\n", kp_data->cur_keycode);
     	}
 		
     	
@@ -139,24 +139,24 @@ static inline void kbd_software_mode_remote_send_key(unsigned long data)
 static inline void kbd_software_mode_remote_data(unsigned long data)
 {
 	unsigned short pulse_width;
-	struct apollo_kp *apollo_kp_data = (struct apollo_kp *) data;
+	struct kp *kp_data = (struct kp *) data;
        
     	pulse_width     = get_pulse_width(data) ;
-	apollo_kp_data->step   = REMOTE_STATUS_DATA ;
+	kp_data->step   = REMOTE_STATUS_DATA ;
 	
-	if((pulse_width > apollo_kp_data->time_window[2]) && (pulse_width < apollo_kp_data->time_window[3])) {
-            	apollo_kp_data->bit_num--;
+	if((pulse_width > kp_data->time_window[2]) && (pulse_width < kp_data->time_window[3])) {
+            	kp_data->bit_num--;
 	}
-	else if((pulse_width > apollo_kp_data->time_window[4]) && (pulse_width < apollo_kp_data->time_window[5])) {
-        	apollo_kp_data->cur_keycode |= 1<<(apollo_kp_data->bit_count-apollo_kp_data->bit_num) ;       //1
-        	apollo_kp_data->bit_num--;
+	else if((pulse_width > kp_data->time_window[4]) && (pulse_width < kp_data->time_window[5])) {
+        	kp_data->cur_keycode |= 1<<(kp_data->bit_count-kp_data->bit_num) ;       //1
+        	kp_data->bit_num--;
 	}
     	else {
-      		apollo_kp_data->step   = REMOTE_STATUS_WAIT ;
+      		kp_data->step   = REMOTE_STATUS_WAIT ;
     	}
-    	if(apollo_kp_data->bit_num == 0)
+    	if(kp_data->bit_num == 0)
     	{
-     	 	apollo_kp_data->repeate_flag= 0;
+     	 	kp_data->repeate_flag= 0;
 		kbd_software_mode_remote_send_key(data);
     	}
 	
@@ -164,33 +164,33 @@ static inline void kbd_software_mode_remote_data(unsigned long data)
 static inline void kbd_software_mode_remote_sync(unsigned long data)
 {
 	unsigned short pulse_width;
-	struct apollo_kp *apollo_kp_data = (struct apollo_kp *) data;
+	struct kp *kp_data = (struct kp *) data;
     
    	pulse_width     = get_pulse_width(data) ;
 	
-	if((pulse_width > apollo_kp_data->time_window[6]) && (pulse_width < apollo_kp_data->time_window[7])) {
-        	apollo_kp_data->repeate_flag=1;
-              if(apollo_kp_data->repeat_enable)
+	if((pulse_width > kp_data->time_window[6]) && (pulse_width < kp_data->time_window[7])) {
+        	kp_data->repeate_flag=1;
+              if(kp_data->repeat_enable)
     	            kbd_software_mode_remote_send_key(data);
               else{
-                    apollo_kp_data->step  = REMOTE_STATUS_SYNC ;
+                    kp_data->step  = REMOTE_STATUS_SYNC ;
                     return;
                 }
 	}
-    	apollo_kp_data->step  = REMOTE_STATUS_SYNC ;
-	apollo_kp_data->timer.data=(unsigned long)apollo_kp_data;
-	mod_timer(&apollo_kp_data->timer,jiffies+msecs_to_jiffies(apollo_kp_data->release_delay));//6
+    	kp_data->step  = REMOTE_STATUS_SYNC ;
+	kp_data->timer.data=(unsigned long)kp_data;
+	mod_timer(&kp_data->timer,jiffies+msecs_to_jiffies(kp_data->release_delay));//6
 	
 }
-void apollo_kp_sw_reprot_key(unsigned long data)
+void kp_sw_reprot_key(unsigned long data)
 {
-	struct apollo_kp *apollo_kp_data = (struct apollo_kp *) data;
+	struct kp *kp_data = (struct kp *) data;
 	int	   current_jiffies=jiffies;
-    	if((current_jiffies -apollo_kp_data->last_jiffies > 12) && (apollo_kp_data->step  <=  REMOTE_STATUS_SYNC)) {
-      		  apollo_kp_data->step = REMOTE_STATUS_WAIT ;
+    	if((current_jiffies -kp_data->last_jiffies > 12) && (kp_data->step  <=  REMOTE_STATUS_SYNC)) {
+      		  kp_data->step = REMOTE_STATUS_WAIT ;
     	}
-    	apollo_kp_data->last_jiffies = current_jiffies ;  //ignore a little nsecs.
-	switch( apollo_kp_data->step)
+    	kp_data->last_jiffies = current_jiffies ;  //ignore a little nsecs.
+	switch( kp_data->step)
     	{
         case REMOTE_STATUS_WAIT:
             kbd_software_mode_remote_wait(data) ;
