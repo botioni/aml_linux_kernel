42ed271f20c2e591bcc4184775211f5bfa7b1205
jerry_yu <jerry_yu@34fca4b9-5ec2-4a8b-9ba4-7e5d7900cddd>
2010-07-29 03:17:55 +0000

    NAND flash
    git-svn-id: https://10.8.9.8/svn/Project_ARMLinux/trunk/kernel/v2.6.34@169 34fca4b9-5ec2-4a8b-9ba4-7e5d7900cddd

--- arch/arm/mach-meson/board-8626m.c
+++ arch/arm/mach-meson/board-8626m.c
@@ -33,6 +33,7 @@
 #include <asm/memory.h>
 #include <asm/mach/map.h>
 #include <mach/am_eth_pinmux.h>
+#include <mach/nand.h>
 #ifdef CONFIG_CACHE_L2X0
 #include <asm/hardware/cache-l2x0.h>
 #endif
@@ -207,7 +208,67 @@ static struct platform_device audiodsp_device = {
     .resource      = audiodsp_resources,
 };
 #endif
+#ifdef CONFIG_AM_NAND
+static struct mtd_partition partition_info[] = 
+{
+	/*{
+		.name = "U-BOOT",
+		.offset = 0,
+		.size=2*1024*1024,
+	//	.set_flags=0,
+	//	.dual_partnum=0,
+	},*/
+	{
+		.name = "Kernel",
+		.offset = 2*1024*1024,
+		.size = 4 * 1024*1024,
+	//	.set_flags=0,
+	//	.dual_partnum=0,
+	},
+	{
+		.name = "YAFFS2",
+		.offset = 2*1024*1024+4 * 1024*1024,
+		.size = 20 * 0x100000,
+	//	.set_flags=0,
+	//	.dual_partnum=0,
+	},
+	{	.name="FTL_Part",
+		.offset=MTDPART_OFS_APPEND,
+		.size=MTDPART_SIZ_FULL,
+	//	.set_flags=MTD_AVNFTL,
+	//	.dual_partnum=1,
+	}
+};
 
+static struct aml_m1_nand_platform aml_nand_platform = {
+	.page_size = 2048,
+	.spare_size=64,
+	.bch_mode=1,			//BCH8
+	.encode_size=528,
+	.timing_mode=5,
+	.ce_num=1,
+	.partitions = partition_info,
+	.nr_partitions = ARRAY_SIZE(partition_info),
+};
+
+static struct resource aml_nand_resources[] = {
+	{
+		.start = 0xc1108600,
+		.end = 0xc1108624,
+		.flags = IORESOURCE_MEM,
+	},
+};
+
+static struct platform_device aml_nand_device = {
+	.name = "aml_m1_nand",
+	.id = 0,
+	.num_resources = ARRAY_SIZE(aml_nand_resources),
+	.resource = aml_nand_resources,
+	.dev = {
+		.platform_data = &aml_nand_platform,
+	},
+};
+#endif
 
 static struct platform_device __initdata *platform_devs[] = {
     #if defined(CONFIG_JPEGLOGO)
@@ -231,6 +292,9 @@ static struct platform_device __initdata *platform_devs[] = {
     #if defined(CONFIG_AMLOGIC_SPI_NOR)
     	&amlogic_spi_nor_device,
     #endif
+    #if defined(CONFIG_AM_NAND)
+		&aml_nand_device,
+    #endif		
 };
 
 static void __init eth_pinmux_init(void)
