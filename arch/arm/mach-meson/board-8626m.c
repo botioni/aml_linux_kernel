d82599be6870e5a858435fb1c2405887f9e42e2c
Jerry Cao-CA <jcao@mooncake.amlogic.com>
2010-08-05 16:22:55 -0700

    Disable USB port A to make port B to work.

--- arch/arm/mach-meson/board-8626m.c
+++ arch/arm/mach-meson/board-8626m.c
@@ -529,7 +529,7 @@ static __init void m1_init_machine(void)
 	/* todo: load device drivers */
 #ifdef CONFIG_USB_DWC_OTG_HCD
 	set_usb_phy_clk(USB_PHY_CLOCK_SEL_XTAL_DIV2);
-	lm_device_register(&usb_ld_a);
+	//lm_device_register(&usb_ld_a);
 	lm_device_register(&usb_ld_b);
 #endif
 #ifdef CONFIG_SATA_DWC_AHCI
