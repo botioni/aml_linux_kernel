776f72a7f9898be61b8d9a95319f2cb796bf0e4c
zhi_zhou <zhi_zhou@34fca4b9-5ec2-4a8b-9ba4-7e5d7900cddd>
2010-08-18 09:58:10 +0000

    fixed the dmod pll to auto load the XTAL value
    git-svn-id: https://10.8.10.5/svn/Project_ARMLinux/trunk/kernel/v2.6.34@249 34fca4b9-5ec2-4a8b-9ba4-7e5d7900cddd

--- arch/arm/mach-meson/board-6236m.c
+++ arch/arm/mach-meson/board-6236m.c
@@ -212,7 +212,7 @@ static void __init device_pinmux_init(void )
 static void __init  device_clk_setting(void)
 {
 	/*Demod CLK for eth and sata*/
-	demod_apll_setting(24*CLK_1M,1200*CLK_1M);
+	demod_apll_setting(0,1200*CLK_1M);
 	/*eth clk*/
     	eth_clk_set(ETH_CLKSRC_APLL_CLK,400*CLK_1M,50*CLK_1M);
 }
