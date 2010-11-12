de87a44bf6f5615d6ccd7569f8dbf5c2abc91fa6
robinzhu <robinzhu@ubuntu.ubuntu-domain>
2010-11-11 22:25:27 -0800

    add free_irq and disable_irq to remove run time warning.

--- drivers/amlogic/input/key_input/key_input.c
+++ drivers/amlogic/input/key_input/key_input.c
@@ -199,7 +199,7 @@ static int register_key_input_dev(struct key_input  *ki_data)
     return ret;
 }
 
-static irqreturn_t am_key_interrupt(int irq, void *dev, struct pt_regs *regs){
+static irqreturn_t am_key_interrupt(int irq, void *dev){
 	printk("=== am_key_interrupt RTC_ADDR0(%x) RTC_ADDR1(%x) ===\n", READ_CBUS_REG(RTC_ADDR0), READ_CBUS_REG(RTC_ADDR1) );
 	WRITE_CBUS_REG(RTC_ADDR1, (READ_CBUS_REG(RTC_ADDR1) | (0x00008000)));
 	return IRQ_HANDLED;
@@ -281,7 +281,7 @@ static int __init key_input_probe(struct platform_device *pdev)
         goto    CATCH_ERR;
     }
 
-    request_irq(INT_RTC, (irq_handler_t) am_key_interrupt, IRQF_SHARED,"power key", pdev);
+    request_irq(INT_RTC, (irq_handler_t) am_key_interrupt, IRQF_SHARED, "power key", NULL);
     WRITE_CBUS_REG(RTC_ADDR0, (READ_CBUS_REG(RTC_ADDR0) | (0x00008000)));
     enable_irq(INT_RTC);
 
@@ -316,7 +316,8 @@ CATCH_ERR:
 static int key_input_remove(struct platform_device *pdev)
 {
     struct key_input *ki_data = platform_get_drvdata(pdev);
-
+    disable_irq(INT_RTC);
+	free_irq(INT_RTC, am_key_interrupt);
     input_unregister_device(ki_data->input);
     input_free_device(ki_data->input);
     unregister_chrdev(ki_data->major,ki_data->name);
