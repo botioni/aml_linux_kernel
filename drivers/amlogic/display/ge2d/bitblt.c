c05dc5a55c48bfa65e5f7d758fe8189e47884220
wangjf <jianfeng.wang@amlogic.com>
2010-12-02 15:23:53 +0800

    add ge2d ex config support for 3d TV

--- drivers/amlogic/display/ge2d/bitblt.c
+++ drivers/amlogic/display/ge2d/bitblt.c
@@ -64,7 +64,7 @@ static void inline _bitblt_noalpha(ge2d_context_t *wq,
     ge2d_cmd_cfg->color_blend_mode = OPERATION_LOGIC;
     ge2d_cmd_cfg->color_logic_op   = LOGIC_OPERATION_COPY;  
     ge2d_cmd_cfg->alpha_blend_mode = OPERATION_LOGIC;
-    ge2d_cmd_cfg->alpha_logic_op   = LOGIC_OPERATION_CLEAR; 
+    ge2d_cmd_cfg->alpha_logic_op   = LOGIC_OPERATION_SET; 
     ge2d_cmd_cfg->wait_done_flag   = block;
 
     ge2d_wq_add_work(wq);
