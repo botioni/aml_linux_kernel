e59936801a314cae53c31dbff85dd212b6e84a3b
niko.he <niko.he@amlogic.com>
2010-12-04 17:46:05 +0800

    improve stkxxx mt-touch performance

--- drivers/amlogic/input/touch_screen/itk.c
+++ drivers/amlogic/input/touch_screen/itk.c
@@ -1,8 +1,6 @@
 /*
  * linux/drivers/input/touchscreen/itk.c
  *
- * Copyright (C) 2007-2008 Avionic Design Development GmbH
- * Copyright (C) 2008-2009 Avionic Design GmbH
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
