6d3ae1f54fe5fe4b084a005f018094594e7dd369
Heming.Lv <Heming.Lv@amlogic.com>
2010-12-30 15:01:09 +0800

    fix bug for refb10

--- arch/arm/mach-meson/board-8726m-refb10.c
+++ arch/arm/mach-meson/board-8726m-refb10.c
@@ -47,7 +47,7 @@
 #include <mach/gpio.h>
 #include <linux/delay.h>
 #include <mach/clk_set.h>
-#include "board-8726m-g7.h"
+#include "board-8726m-refb10.h"
 
 #if defined(CONFIG_TOUCHSCREEN_ADS7846)
 #include <linux/spi/spi.h>
