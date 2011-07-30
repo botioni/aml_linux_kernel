16b28ebef0a28e7e757d7be702434edcc9ede953
larson <larson.jiang@amlogic.com>
2011-07-30 09:13:51 +0800

    fix 32K output problem

--- arch/arm/mach-meson3/board-m3-reff02.c
+++ arch/arm/mach-meson3/board-m3-reff02.c
@@ -2245,6 +2245,7 @@ static void __init device_pinmux_init(void )
     set_audio_pinmux(AUDIO_IN_JTAG);
     
     //set clk for wifi
+    WRITE_CBUS_REG(HHI_GEN_CLK_CNTL,(READ_CBUS_REG(HHI_GEN_CLK_CNTL)&(~(0x7f<<0)))|(1<<0)|(1<<8)|(7<<9));
 	CLEAR_CBUS_REG_MASK(PREG_PAD_GPIO2_EN_N, (1<<15));    
     SET_CBUS_REG_MASK(PERIPHS_PIN_MUX_3, (1<<22));
 
