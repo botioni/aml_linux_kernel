5fe468fb8e409723be777f4a92470d61e2149bf3
ivan.nie <ivan.nie@amlogic.com>
2011-09-07 13:55:21 +0800

    change display drvier by Evoke.zhang

--- drivers/amlogic/display/vout/lcd_ttl/refb13.c
+++ drivers/amlogic/display/vout/lcd_ttl/refb13.c
@@ -33,99 +33,83 @@
 #include <mach/am_regs.h>
 #include <mach/pinmux.h>
 #include <mach/power_gate.h>
-//INNOLUX AT070TN93 V.2
+
 #define LCD_WIDTH       800 
-#define LCD_HEIGHT      480
-#define MAX_WIDTH       928
-#define MAX_HEIGHT      525
+#define LCD_HEIGHT      600
+#define MAX_WIDTH       1056
+#define MAX_HEIGHT      631
 #define VIDEO_ON_LINE   22
 
 #define BL_ON            1
 #define BL_OFF           0
 static void t13_power_on(void);
 static void t13_power_off(void);
+void power_on_backlight(void);
+void power_off_backlight(void);
 #ifdef CONFIG_AM_LOGO
 static int bl_state = BL_ON;
 #endif
 
-static tcon_conf_t tcon_config =
+static lcdConfig_t lcd_config =
 {
     .width      = LCD_WIDTH,
     .height     = LCD_HEIGHT,
     .max_width  = MAX_WIDTH,
     .max_height = MAX_HEIGHT,
     .video_on_line = VIDEO_ON_LINE,
-    .pll_ctrl = 0x062d,
+    .pll_ctrl = 0x063c,
     .clk_ctrl = 0x1fc1,
     .gamma_cntl_port = (1 << LCD_GAMMA_EN) | (0 << LCD_GAMMA_RVS_OUT) | (1 << LCD_GAMMA_VCOM_POL),
     .gamma_vcom_hswitch_addr = 0,
     .rgb_base_addr = 0xf0,
     .rgb_coeff_addr = 0x74a,
     .pol_cntl_addr = (0x0 << LCD_CPH1_POL) |(0x1 << LCD_HS_POL) | (0x1 << LCD_VS_POL),
-    .dith_cntl_addr = 0x400,
-    .sth1_hs_addr = 0,
-    .sth1_he_addr = 0,
+    .dith_cntl_addr = 0x400,      //For 8 bits    //0x600   //For 6 bits
+    .sth1_hs_addr = 64,
+    .sth1_he_addr = 73,
     .sth1_vs_addr = 0,
-    .sth1_ve_addr = 0,
-    .sth2_hs_addr = 0,
-    .sth2_he_addr = 0,
-    .sth2_vs_addr = 0,
-    .sth2_ve_addr = 0,
+    .sth1_ve_addr = MAX_HEIGHT - 1,    
     .oeh_hs_addr = 67,
-    .oeh_he_addr = 67+LCD_WIDTH-1,
+    .oeh_he_addr = 67+LCD_WIDTH,
     .oeh_vs_addr = VIDEO_ON_LINE,
     .oeh_ve_addr = VIDEO_ON_LINE+LCD_HEIGHT-1,
     .vcom_hswitch_addr = 0,
     .vcom_vs_addr = 0,
     .vcom_ve_addr = 0,
-    .cpv1_hs_addr = 0,
-    .cpv1_he_addr = 0,
+    .cpv1_hs_addr = 40,
+    .cpv1_he_addr = 560,
     .cpv1_vs_addr = 0,
-    .cpv1_ve_addr = 0,
-    .cpv2_hs_addr = 0,
-    .cpv2_he_addr = 0,
-    .cpv2_vs_addr = 0,
-    .cpv2_ve_addr = 0,
+    .cpv1_ve_addr = MAX_HEIGHT - 1,    
     .stv1_hs_addr = 0,
-    .stv1_he_addr = 0,
-    .stv1_vs_addr = 0,
-    .stv1_ve_addr = 0,
-    .stv2_hs_addr = 0,
-    .stv2_he_addr = 0,
-    .stv2_vs_addr = 0,
-    .stv2_ve_addr = 0,
-    .oev1_hs_addr = 0,
-    .oev1_he_addr = 0,
+    .stv1_he_addr = MAX_WIDTH - 1,
+    .stv1_vs_addr = VIDEO_ON_LINE,
+    .stv1_ve_addr = VIDEO_ON_LINE,    
+    .oev1_hs_addr = 20,
+    .oev1_he_addr = 66,
     .oev1_vs_addr = 0,
-    .oev1_ve_addr = 0,
-    .oev2_hs_addr = 0,
-    .oev2_he_addr = 0,
-    .oev2_vs_addr = 0,
-    .oev2_ve_addr = 0,
-    .oev3_hs_addr = 0,
-    .oev3_he_addr = 0,
-    .oev3_vs_addr = 0,
-    .oev3_ve_addr = 0,
+    .oev1_ve_addr = MAX_HEIGHT - 1,    
     .inv_cnt_addr = (0<<LCD_INV_EN) | (0<<LCD_INV_CNT),
     .tcon_misc_sel_addr = (1<<LCD_STV1_SEL) | (1<<LCD_STV2_SEL),
     .dual_port_cntl_addr = (1<<LCD_TTL_SEL) | (1<<LCD_ANALOG_SEL_CPH3) | (1<<LCD_ANALOG_3PHI_CLK_SEL),
     .flags = 0,
-    .screen_width = 5,
+    .screen_width = 4,
     .screen_height = 3,
-    .sync_duration_num = 308,
-    .sync_duration_den = 5,
+    .sync_duration_num = 60,
+    .sync_duration_den = 1,
     .power_on=t13_power_on,
     .power_off=t13_power_off,
+    .backlight_on = power_on_backlight,
+    .backlight_off = power_off_backlight,
 };
-static struct resource tcon_resources[] = {
+static struct resource lcd_resources[] = {
     [0] = {
-        .start = (ulong)&tcon_config,
-        .end   = (ulong)&tcon_config + sizeof(tcon_conf_t) - 1,
+        .start = (ulong)&lcd_config,
+        .end   = (ulong)&lcd_config + sizeof(lcdConfig_t) - 1,
         .flags = IORESOURCE_MEM,
     },
 };
 
-static void t13_setup_gama_table(tcon_conf_t *pConf)
+static void t13_setup_gama_table(lcdConfig_t *pConf)
 {
     int i;
     const unsigned short gamma_adjust[256] = {
@@ -146,24 +130,27 @@ static void t13_setup_gama_table(tcon_conf_t *pConf)
     }
 }
 
-#define PWM_MAX		40000   //set pwm_freq=24MHz/PWM_MAX (Base on crystal frequence: 24MHz, 0<PWM_MAX<65535)
 void power_on_backlight(void)
 {
-    //BL_PWM -> GPIOA_7 -> PWM_A
+    int i;
 #ifdef CONFIG_AM_LOGO    
     if(bl_state == BL_ON)
         return;
     bl_state = BL_ON;  
 #endif      
-    msleep(200);
-    WRITE_MPEG_REG(0x2154, (PWM_MAX<<16) | (0<<0));
-	WRITE_MPEG_REG(0x2156, (READ_MPEG_REG(0x202e) & ~(1<<2)) | (1<<0));
-	
-	WRITE_MPEG_REG(0x202c, READ_MPEG_REG(0x202c) & ~((1<<9)|(1<<22)));
-	WRITE_MPEG_REG(0x202e, READ_MPEG_REG(0x202e) & ~(1<<29));
-	WRITE_MPEG_REG(0x2035, READ_MPEG_REG(0x2035) & ~(1<<22));
-	WRITE_MPEG_REG(0x2038, READ_MPEG_REG(0x2038) & ~(1<<7));
-	WRITE_MPEG_REG(0x202e, READ_MPEG_REG(0x202e) | (1<<31));
+
+    i=100;
+    while(i--)
+        udelay(1000);
+    SET_CBUS_REG_MASK(PWM_MISC_REG_AB, (1 << 0)); 
+    i=100;
+    while(i--)
+        udelay(1000);    
+    SET_CBUS_REG_MASK(PERIPHS_PIN_MUX_2, (1<<31)); 
+
+    /* PIN28, GPIOA_6, Pull high, For En_5V */
+    set_gpio_val(GPIOA_bank_bit(6), GPIOA_bit_bit0_14(6), 1);
+    set_gpio_mode(GPIOA_bank_bit(6), GPIOA_bit_bit0_14(6), GPIO_OUTPUT_MODE);
 }
 
 void power_off_backlight(void)
@@ -173,61 +160,68 @@ void power_off_backlight(void)
         return;
     bl_state = BL_OFF;
 #endif    
-    //BL_PWM -> GPIOA_7: 0
-    WRITE_MPEG_REG(0x2154, (0<<16) | (PWM_MAX<<0));
-}
 
+    /* PIN31, GPIOA_7, Pull high, BL_PWM Enable*/
+    CLEAR_CBUS_REG_MASK(PERIPHS_PIN_MUX_2, (1<<31)); 
+    set_gpio_val(GPIOA_bank_bit(7), GPIOA_bit_bit0_14(7), 0);
+    set_gpio_mode(GPIOA_bank_bit(7), GPIOA_bit_bit0_14(7), GPIO_OUTPUT_MODE);
+    CLEAR_CBUS_REG_MASK(PWM_MISC_REG_AB, (1 << 0)); 
+
+    /* PIN28, GPIOA_6, Pull low, For En_5v */
+    //set_gpio_val(GPIOA_bank_bit(6), GPIOA_bit_bit0_14(6), 0);
+    //set_gpio_mode(GPIOA_bank_bit(6), GPIOA_bit_bit0_14(6), GPIO_OUTPUT_MODE);
+}
 
 static void power_on_lcd(void)
 {
-    //LCD_3.3V -> TCON_VCOM(GPIOA_6): 0
-	WRITE_MPEG_REG(0x200d, READ_MPEG_REG(0x200d) & ~(1<<10));
-	WRITE_MPEG_REG(0x200c, READ_MPEG_REG(0x200c) & ~(1<<10));
-	msleep(20);
-	
-	//AVDD -> TCON_OEV1(GPIOA_4): 1
-	WRITE_MPEG_REG(0x200d, READ_MPEG_REG(0x200d) | (1<<8));
-	WRITE_MPEG_REG(0x200c, READ_MPEG_REG(0x200c) & ~(1<<8));
-	msleep(5);
-	
-	//VGL -> GPIOD_17: 1
-	WRITE_MPEG_REG(0x2013, READ_MPEG_REG(0x2013) | (1<<15));
-	WRITE_MPEG_REG(0x2012, READ_MPEG_REG(0x2012) & ~(1<<15));
-	msleep(15);
-	
-	//VGH -> TCON_CPV1(GPIOA_3): 1
-	WRITE_MPEG_REG(0x200d, READ_MPEG_REG(0x200d) | (1<<7));
-	WRITE_MPEG_REG(0x200c, READ_MPEG_REG(0x200c) & ~(1<<7));	
+    /* GPIOA_3, Pull low, For LCD_3.3V */
+    set_gpio_val(GPIOA_bank_bit(3), GPIOA_bit_bit0_14(3), 0);
+    set_gpio_mode(GPIOA_bank_bit(3), GPIOA_bit_bit0_14(3), GPIO_OUTPUT_MODE);   
+    udelay(1000);     
+
+    /* PIN172, GPIOC_3, Pull high, For AVDD */
+    set_gpio_val(GPIOC_bank_bit0_26(3), GPIOC_bit_bit0_26(3), 1);
+    set_gpio_mode(GPIOC_bank_bit0_26(3), GPIOC_bit_bit0_26(3), GPIO_OUTPUT_MODE);
+    udelay(1000);
+    CLK_GATE_ON(LCD);    
+    set_mio_mux(4,(0x3f<<0));
+    set_mio_mux(0, 1<<11);
+    set_mio_mux(0, 1<<14);
+    udelay(1000);
+
 }
 
 static void power_off_lcd(void)
 {
-	//VGH -> TCON_CPV1(GPIOA_3): 0
-	WRITE_MPEG_REG(0x200d, READ_MPEG_REG(0x200d) & ~(1<<7));
-	WRITE_MPEG_REG(0x200c, READ_MPEG_REG(0x200c) & ~(1<<7));
-	msleep(15);
-	
-	//VGL -> GPIOD_17: 0
-	WRITE_MPEG_REG(0x2013, READ_MPEG_REG(0x2013) & ~(1<<15));
-	WRITE_MPEG_REG(0x2012, READ_MPEG_REG(0x2012) & ~(1<<15));
-	msleep(5);
-	
-	//AVDD -> TCON_OEV1(GPIOA_4): 0
-	WRITE_MPEG_REG(0x200d, READ_MPEG_REG(0x200d) & ~(1<<8));
-	WRITE_MPEG_REG(0x200c, READ_MPEG_REG(0x200c) & ~(1<<8));
-	msleep(20);
-	
-	//LCD_3.3V -> TCON_VCOM(GPIOA_6): 1
-	WRITE_MPEG_REG(0x200d, READ_MPEG_REG(0x200d) | (1<<10));
-	WRITE_MPEG_REG(0x200c, READ_MPEG_REG(0x200c) & ~(1<<10));	
+    /* PIN172, GPIOC_3, Pull high, For AVDD */
+    set_gpio_val(GPIOC_bank_bit0_26(3), GPIOC_bit_bit0_26(3), 0);
+    set_gpio_mode(GPIOC_bank_bit0_26(3), GPIOC_bit_bit0_26(3), GPIO_OUTPUT_MODE);
+
+    /* GPIOA_3, Pull hi, power down LCD_3.3V */
+    set_gpio_val(GPIOA_bank_bit(3), GPIOA_bit_bit0_14(3), 1);
+    set_gpio_mode(GPIOA_bank_bit(3), GPIOA_bit_bit0_14(3), GPIO_OUTPUT_MODE);   
+    CLK_GATE_OFF(LCD);     
+    clear_mio_mux(4,(0x3f<<0));
+    clear_mio_mux(0, 1<<11);
+    clear_mio_mux(0, 1<<14);        
+
 }
 
 static void set_tcon_pinmux(void)
 {
     /* TCON control pins pinmux */
-    /* GPIOA_5 -> LCD_Clk, GPIOA_2 -> TCON_OEH */
-    set_mio_mux(0, ((1<<11)|(1<<14)));
-    set_mio_mux(4,(3<<0)|(3<<2)|(3<<4));   //For 8bits
+    
+    /* GPIOD_12 --> U/D */
+    set_gpio_val(GPIOC_bank_bit0_26(12), GPIOC_bit_bit0_26(12), 0);
+    set_gpio_mode(GPIOC_bank_bit0_26(12), GPIOC_bit_bit0_26(12), GPIO_OUTPUT_MODE);
+
+    /* GPIOA_5 --> LCD_Clk, GPIOA_2 --> TCON_OEH, */
+    set_mio_mux(0, 1<<11);
+    set_mio_mux(0, 1<<14);
+
+    /* RGB data pins */
+    set_mio_mux(4,(0x3f<<0));   //For 8bits
+//    set_mio_mux(4,(0x1<<0)|(1<<2)|(1<<4));   //For 6bits
 
 }
 static void t13_power_on(void)
@@ -268,19 +262,19 @@ void Power_on_bl(void)
 }
 EXPORT_SYMBOL(Power_on_bl);
 #endif
-static struct platform_device tcon_dev = {
+static struct platform_device lcd_dev = {
     .name = "tcon-dev",
     .id   = 0,
-    .num_resources = ARRAY_SIZE(tcon_resources),
-    .resource      = tcon_resources,
+    .num_resources = ARRAY_SIZE(lcd_resources),
+    .resource      = lcd_resources,
 };
 
 static int __init t13_init(void)
 {
-    t13_setup_gama_table(&tcon_config);
+    t13_setup_gama_table(&lcd_config);
     t13_io_init();
 
-    platform_device_register(&tcon_dev);
+    platform_device_register(&lcd_dev);
 
     return 0;
 }
@@ -290,10 +284,10 @@ static void __exit t13_exit(void)
     power_off_backlight();
     power_off_lcd();
 
-    platform_device_unregister(&tcon_dev);
+    platform_device_unregister(&lcd_dev);
 }
 
-subsys_initcall(t13_init);
+arch_initcall(t13_init);
 //module_init(t13_init);
 module_exit(t13_exit);
 
