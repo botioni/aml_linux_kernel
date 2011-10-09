3ad59d4717c93aaef3deb1153bf0a8183fb6eefe
Sandy lUo <sandy.luo@amlogic.com>
2011-10-09 16:35:47 +0800

    change osd buffer size

--- arch/arm/mach-meson3/board-m3-reff06.h
+++ arch/arm/mach-meson3/board-m3-reff06.h
@@ -26,7 +26,7 @@
 #define OSD_576_PIX			(768*576)
 #define OSD_720_PIX			(1280*720)
 #define OSD_1080_PIX		(1920*1080)
-#define OSD_PANEL_PIX		(800*600)
+#define OSD_PANEL_PIX		(1280*768)
 #define B16BpP	(2)
 #define B32BpP	(4)
 #define DOUBLE_BUFFER	(2)
