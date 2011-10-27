177e7bc9fac91810016b9a88f5fe26488901ecc8
zongdong.jiao <zongdong.jiao@amlogic.com>
2011-10-27 12:07:44 +0800

    Reduce hdmi_pll_vcc power consumption when no hdmi output

--- drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_module.h
+++ drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_module.h
@@ -90,7 +90,7 @@ typedef struct hdmi_tx_dev_s {
 #define HDMI_AUDIO_CONTENT_PROTECTION   5
 
 
-#define HDMITX_VER "2011Oct14a"
+#define HDMITX_VER "2011Oct27a"
 /************************************
 *    hdmitx protocol level interface
 *************************************/
