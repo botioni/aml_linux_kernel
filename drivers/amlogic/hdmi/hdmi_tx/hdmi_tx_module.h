231f50ea506afa4df7a3e7564032f385f3274555
zongdong.jiao <zongdong.jiao@amlogic.com>
2011-11-10 19:29:20 +0800

    Deeply reduce HDMI_PLL_AVDD consumption when no HDMI output

--- drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_module.h
+++ drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_module.h
@@ -90,7 +90,7 @@ typedef struct hdmi_tx_dev_s {
 #define HDMI_AUDIO_CONTENT_PROTECTION   5
 
 
-#define HDMITX_VER "2011Oct27a"
+#define HDMITX_VER "2011Nov10"
 /************************************
 *    hdmitx protocol level interface
 *************************************/
