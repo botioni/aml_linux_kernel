738af6d87cd5e6c89a38405d8addfa273ee3e2f6
Sandy lUo <sandy.luo@amlogic.com>
2011-11-11 14:22:00 +0800

    revert itk touch driver for sunny.luo

--- drivers/amlogic/input/touch_screen/itk.c
+++ drivers/amlogic/input/touch_screen/itk.c
@@ -26,10 +26,6 @@ static struct early_suspend itk_early_suspend;
 #endif
 
 // definition
-#define TS_IDLE_TIME (400 * 1000000)
-#define TS_SLEEP_TIME (200 * 1000000)
-#define TS_WAKEUP_TIME (200 * 1000000)
-
 #define ILITEK_I2C_RETRY_COUNT			3
 #define ILITEK_I2C_DEFAULT_ADDRESS      0x41
 #define ILITEK_I2C_DRIVER_NAME			"ilitek_i2c"
@@ -583,23 +579,6 @@ static void itk_work(struct work_struct *work)
 		return;
 	}
 
-	spin_lock(&ts->lock);	
-	if (ts->running == 2) {
-		ts->pdata->touch_on(0);
-		hrtimer_start(&ts->timer, ktime_set(0, TS_SLEEP_TIME), HRTIMER_MODE_REL);
-		ts->running = 3;
-		spin_unlock(&ts->lock);
-		return;
-	}
-	else if (ts->running == 3) {
-		ts->pdata->touch_on(1);
-		hrtimer_start(&ts->timer, ktime_set(0, TS_WAKEUP_TIME), HRTIMER_MODE_REL);
-		ts->running = 2;
-		spin_unlock(&ts->lock);
-		return;
-	}
-	spin_unlock(&ts->lock);	
-
     if (itk_get_pendown_state(ts)) 
     {
     		ret = itk_read_sensor(ts);
@@ -607,7 +586,7 @@ static void itk_work(struct work_struct *work)
             printk(KERN_INFO "work read i2c failed\n");
             goto restart;
         }
-        else if(ret == 2)  //有手超過邊界...所有點都不上報
+        else if(ret == 2)  //|3∟a?W1L???谷...?辰|3?I3㏒∟㏒∟W3?
         {
         	ts->touching_num = 0;
         }
@@ -712,9 +691,7 @@ restart:
         }
         ts->touching_num = 0;
         enable_irq(ts->client->irq);
-				printk("start idle timer\n");
-        hrtimer_start(&ts->timer, ktime_set(0, TS_IDLE_TIME), HRTIMER_MODE_REL);
-				ts->running	= 2;
+		ts->running	= 0;
     }
 }
 
@@ -727,7 +704,7 @@ static enum hrtimer_restart itk_timer(struct hrtimer *timer)
 {
     struct itk *ts = container_of(timer, struct itk, timer);
     unsigned long flags = 0;
-//    ts->running	= 1;
+    ts->running	= 1;
     spin_lock_irqsave(&ts->lock, flags);
 //  printk(KERN_INFO "enter timer\n");
     queue_work(ts->workqueue, &ts->work);
