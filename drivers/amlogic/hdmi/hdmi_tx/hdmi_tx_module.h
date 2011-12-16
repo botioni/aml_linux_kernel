a4c30e82833870224052a223c741be057b530081
zongdong.jiao <zongdong.jiao@amlogic.com>
2011-12-16 17:27:19 +0800

    update hdmi driver version

--- drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_module.h
+++ drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_module.h
@@ -90,7 +90,7 @@ typedef struct hdmi_tx_dev_s {
 #define HDMI_AUDIO_CONTENT_PROTECTION   5
 
 
-#define HDMITX_VER "2011Dec12a"
+#define HDMITX_VER "2011Dec16a"
 /************************************
 *    hdmitx protocol level interface
 *************************************/
