d037944483b4fa15bcec03fc871aa71f25b1db50
Victor Wan <victor.wan@amlogic.com>
2011-12-20 12:38:30 +0800

    usb: fix adb/ms tranfer issue

--- drivers/amlogic/usb/dwc_otg/dwc_otg_pcd_intr.c
+++ drivers/amlogic/usb/dwc_otg/dwc_otg_pcd_intr.c
@@ -159,11 +159,22 @@ void start_next_request(dwc_otg_pcd_ep_t * _ep)
 {
 	dwc_otg_pcd_request_t *req = 0;
 
+	if (!list_empty(&_ep->pcd->req_queue) && _ep->dwc_ep.num && _ep->dwc_ep.is_in){
+		req = list_entry(_ep->pcd->req_queue.next, dwc_otg_pcd_request_t, pcd_queue); 
+		_ep = req->ep;
+		DWC_DEBUGPL(DBG_PCD, "ep%d,len=%d,%s\n",_ep->dwc_ep.num,req->req.length,
+			_ep->dwc_ep.is_in?"in":"out");
+	}
+
 	if (!list_empty(&_ep->queue)) {
 		req = list_entry(_ep->queue.next, dwc_otg_pcd_request_t, queue);
 
 		if(GET_CORE_IF(_ep->pcd)->dma_enable)
 			dwc_otg_pcd_dma_map(&_ep->dwc_ep, &req->req);
+
+		if(_ep->dwc_ep.is_in && _ep->dwc_ep.num){
+			_ep->pcd->ep_in_sync = _ep->dwc_ep.num;
+		}
 		/* Setup and start the Transfer */
 		_ep->dwc_ep.start_xfer_buff = req->req.buf;
 		_ep->dwc_ep.xfer_buff = req->req.buf;
