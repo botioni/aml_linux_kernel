27c0c2daf8d301dffcd007e17fc0a9142ed96845
sky.zhou <sky.zhou@droid04.amlogic.com>
2011-12-24 14:08:17 +0800

    fix camera start recording fail

--- drivers/media/video/amlcamera/gc0308.c
+++ drivers/media/video/amlcamera/gc0308.c
@@ -337,7 +337,7 @@ static inline struct gc0308_fh *to_fh(struct gc0308_device *dev)
 
 static struct v4l2_frmsize_discrete gc0308_prev_resolution[2]= //should include 320x240 and 640x480, those two size are used for recording
 {
-	{320,240},
+	{352,288},
 	{640,480},
 };
 
