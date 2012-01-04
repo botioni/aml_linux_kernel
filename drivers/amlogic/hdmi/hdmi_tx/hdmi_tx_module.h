09eb4810b1c5de6a9589ba63a6aa3b65664b9263
zongdong.jiao <zongdong.jiao@amlogic.com>
2012-01-04 22:33:29 +0800

    update new HDMI Physical Settings to compliance 1080p issue chips

--- drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_module.h
+++ drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_module.h
@@ -90,7 +90,7 @@ typedef struct hdmi_tx_dev_s {
 #define HDMI_AUDIO_CONTENT_PROTECTION   5
 
 
-#define HDMITX_VER "2011Dec16a"
+#define HDMITX_VER "2012Jan04a"
 /************************************
 *    hdmitx protocol level interface
 *************************************/
--- drivers/amlogic/hdmi/hdmi_tx/m3/hdmi_tx_hw.c
+++ drivers/amlogic/hdmi/hdmi_tx/m3/hdmi_tx_hw.c
@@ -1075,28 +1075,25 @@ void hdmi_hw_set_powermode( int power_mode, int vic)
             {
                 case HDMI_480p60:
                 case HDMI_480p60_16x9:
-                    hdmi_wr_reg(TX_SYS1_AFE_TEST, 0x7d);    //0x17
-                    hdmi_wr_reg(TX_CORE_CALIB_VALUE,0x6);   //0xf7
-                    hdmi_wr_reg(TX_SYS1_AFE_RESET, 0x2);    //0x16
+                    hdmi_wr_reg(TX_SYS1_AFE_TEST, 0x7f);    //0x17
+                    hdmi_wr_reg(TX_CORE_CALIB_VALUE,0x5);   //0xf7
+                    hdmi_wr_reg(TX_SYS1_AFE_RESET, 0x1);    //0x16
                     hdmi_wr_reg(TX_SYS1_BANDGAP, 0x0);      //0x14
                     hdmi_wr_reg(TX_SYS1_BIAS, 0x0);         //0x15
-                    Wr(HHI_VID_PLL_CNTL3, 0x40e8);
                     break;
                 case HDMI_720p60:
-                    hdmi_wr_reg(TX_SYS1_AFE_TEST, 0x7d);    //0x17
-                    hdmi_wr_reg(TX_CORE_CALIB_VALUE,0x4);   //0xf7
-                    hdmi_wr_reg(TX_SYS1_AFE_RESET, 0x2);    //0x16
+                    hdmi_wr_reg(TX_SYS1_AFE_TEST, 0x7f);    //0x17
+                    hdmi_wr_reg(TX_CORE_CALIB_VALUE,0x3);   //0xf7
+                    hdmi_wr_reg(TX_SYS1_AFE_RESET, 0x1);    //0x16
                     hdmi_wr_reg(TX_SYS1_BANDGAP, 0x0);      //0x14
                     hdmi_wr_reg(TX_SYS1_BIAS, 0x3);         //0x15
-                    Wr(HHI_VID_PLL_CNTL3, 0x40e8);
                     break;
                 case HDMI_1080p60:
-                    hdmi_wr_reg(TX_SYS1_AFE_TEST, 0x7d);    //0x17
-                    hdmi_wr_reg(TX_CORE_CALIB_VALUE,0xd);   //0xf7
-                    hdmi_wr_reg(TX_SYS1_AFE_RESET, 0x3);    //0x16
+                    hdmi_wr_reg(TX_SYS1_AFE_TEST, 0x7f);    //0x17
+                    hdmi_wr_reg(TX_CORE_CALIB_VALUE,0x3);   //0xf7
+                    hdmi_wr_reg(TX_SYS1_AFE_RESET, 0x1);    //0x16
                     hdmi_wr_reg(TX_SYS1_BANDGAP, 0x1);      //0x14 Prem
                     hdmi_wr_reg(TX_SYS1_BIAS, 0x3);         //0x15 Slew
-                    Wr(HHI_VID_PLL_CNTL3, 0xcce8);          //In 1080P case: Increase IBCOR. For other format, keep normal
                     break;
                 default:
                     break;
