7a4d45fa9321b75435b4d265cc07cfcdb38e940d
kevin <=>
2012-02-21 14:14:40 +0800

    Move the ex_size member to reserved area in order compatible.
      ex_size is an extra size of PCM datas in alsa local buffer,
    used when calculating the latency from dsp to HW output. The
    same modification must be done at alsa-lib in android.

--- include/sound/asound.h
+++ include/sound/asound.h
@@ -395,14 +395,14 @@ struct snd_pcm_status {
 	struct timespec trigger_tstamp;	/* time when stream was started/stopped/paused */
 	struct timespec tstamp;		/* reference timestamp */
 	snd_pcm_uframes_t appl_ptr;	/* appl ptr */
-	snd_pcm_uframes_t ex_size;   /* extra size kept not be put into HW buf */
 	snd_pcm_uframes_t hw_ptr;	/* hw ptr */
 	snd_pcm_sframes_t delay;	/* current delay in frames */
 	snd_pcm_uframes_t avail;	/* number of frames available */
 	snd_pcm_uframes_t avail_max;	/* max frames available on hw since last status */
 	snd_pcm_uframes_t overrange;	/* count of ADC (capture) overrange detections from last status */
 	snd_pcm_state_t suspended_state; /* suspended stream state */
-	unsigned char reserved[60];	/* must be filled with zero */
+	snd_pcm_uframes_t ex_size;   /* extra size kept not be put into HW buf */
+    unsigned char reserved[60];	/* must be filled with zero */
 };
 
 struct snd_pcm_mmap_status {
@@ -415,8 +415,8 @@ struct snd_pcm_mmap_status {
 
 struct snd_pcm_mmap_control {
 	snd_pcm_uframes_t appl_ptr;	/* RW: appl ptr (0...boundary-1) */
+    snd_pcm_uframes_t avail_min;	/* RW: min available frames for wakeup */
     snd_pcm_uframes_t ex_size;  /* extra size */
-	snd_pcm_uframes_t avail_min;	/* RW: min available frames for wakeup */
 };
 
 #define SNDRV_PCM_SYNC_PTR_HWSYNC	(1<<0)	/* execute hwsync */
