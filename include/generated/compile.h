17b9af623a1a9d38a32c4a0653936a0c2a922bb8
Lawrence Mok <lawrence.mok@amlogic.com>
2012-02-27 21:36:26 -0800

    ignore generated files

--- include/generated/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 2 */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 Fri Feb 24 21:46:18 CST 2012"
-#define LINUX_COMPILE_TIME "21:46:18"
-#define LINUX_COMPILE_BY "kevin"
-#define LINUX_COMPILE_HOST "kevin-Lenovo"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
