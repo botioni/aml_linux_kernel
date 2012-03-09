6bb635de9c655c645206f995317f6ddb6dbadef2
zongdong.jiao <zongdong.jiao@amlogic.com>
2012-03-10 01:36:06 +0800

    temporary mark HDMI/DVI judge

--- drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_video.c
+++ drivers/amlogic/hdmi/hdmi_tx/hdmi_tx_video.c
@@ -390,16 +390,16 @@ int hdmitx_set_display(hdmitx_dev_t* hdmitx_device, HDMI_Video_Codes_t VideoCode
 //TMDS_MODE[hdmi_config]
 //0: DVI Mode       1: HDMI Mode
             //if(hdmitx_device->hdmi_info.output_state==CABLE_PLUGIN_DVI_OUT)
-            if(is_dvi_device(&hdmitx_device->RXCap))
-            {
-                hdmi_print(1,"Sink is DVI device\n");
-                hdmi_wr_reg(TX_TMDS_MODE, hdmi_rd_reg(TX_TMDS_MODE) & ~(1<<6));
-            }
-            else
-            {
-                hdmi_print(1,"Sink is HDMI device\n");
+//            if(is_dvi_device(&hdmitx_device->RXCap))
+//            {
+//                hdmi_print(1,"Sink is DVI device\n");
+//                hdmi_wr_reg(TX_TMDS_MODE, hdmi_rd_reg(TX_TMDS_MODE) & ~(1<<6));
+//            }
+//            else
+//            {
+//                hdmi_print(1,"Sink is HDMI device\n");
                 hdmi_wr_reg(TX_TMDS_MODE, hdmi_rd_reg(TX_TMDS_MODE) |  (3<<6));
-            }
+//            }
 
 //check system status by reading EDID_STATUS
             switch(hdmi_rd_reg(TX_HDCP_ST_EDID_STATUS) >> 6)
