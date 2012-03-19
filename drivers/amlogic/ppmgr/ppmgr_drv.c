9e65f4c21eda45488caf4ed5652f6fe06d9977c1
kasin.li <kasin.li@amlogic.com>
2012-03-19 17:03:28 +0800

    fix a bug, which can modify orientation when set angle of ppmgr in inctl

--- drivers/amlogic/ppmgr/ppmgr_drv.c
+++ drivers/amlogic/ppmgr/ppmgr_drv.c
@@ -74,10 +74,10 @@ static ssize_t _ppmgr_orientation_write(unsigned long val)
             return -EINVAL;
         }
     }
-    ppmgr_device.orientation = angle;
+    ppmgr_device.angle = angle;
     ppmgr_device.videoangle = (ppmgr_device.angle+ ppmgr_device.orientation)%4;
     printk("angle:%d,orientation:%d,videoangle:%d \n",ppmgr_device.angle ,
-        ppmgr_device.orientation, ppmgr_device.videoangle);
+							ppmgr_device.orientation, ppmgr_device.videoangle);
 
     return 0;
 }
