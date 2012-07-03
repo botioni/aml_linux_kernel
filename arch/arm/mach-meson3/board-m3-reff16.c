0c83b286b58d2268fe05a06a436511d114609bbd
Shijie.rong <shijie.rong@amlogic.com>
2012-07-03 10:36:59 +0800

    add ite9133(DVB-T) demod

--- arch/arm/mach-meson3/board-m3-reff16.c
+++ arch/arm/mach-meson3/board-m3-reff16.c
@@ -1854,6 +1854,48 @@ static  struct platform_device ite9173_device = {
 	.num_resources    = ARRAY_SIZE(ite9173_resource),
 	.resource         = ite9173_resource,
 };
+
+static struct resource ite9133_resource[]  = {
+	[0] = {
+		.start = (GPIOD_bank_bit0_9(8)<<16)|GPIOD_bit_bit0_9(8), //reset pin
+		.end   = (GPIOD_bank_bit0_9(8)<<16)|GPIOD_bit_bit0_9(8),
+		.flags = IORESOURCE_MEM,
+		.name  = "frontend0_reset"
+	},
+	[1] = {
+		.start = 0,                                    //frontend 0 i2c adapter id
+		.end   = 0,
+		.flags = IORESOURCE_MEM,
+		.name  = "frontend0_i2c"
+	},
+	[2] = {
+		.start = 0x98,                                 //frontend 0 tuner address
+		.end   = 0x98,
+		.flags = IORESOURCE_MEM,
+		.name  = "frontend0_tuner_addr"
+	},
+	[3] = {
+		.start =  0x38,                                 //frontend 0 demod address
+		.end   =  0x38,
+		.flags = IORESOURCE_MEM,
+		.name  = "frontend0_demod_addr"
+	},
+	[4] = {
+		.start = (GPIOC_bank_bit0_15(3)<<16)|GPIOC_bank_bit0_15(3),  //// tuner_enable
+		.end   = (GPIOC_bank_bit0_15(3)<<16)|GPIOC_bank_bit0_15(3),
+		.flags = IORESOURCE_MEM,
+		.name  = "frontend0_power"
+	},
+};
+
+static  struct platform_device ite9133_device = {
+	.name             = "ite9133",
+	.id               = -1,
+	.num_resources    = ARRAY_SIZE(ite9133_resource),
+	.resource         = ite9133_resource,
+};
+
+
 #endif
 #if defined(CONFIG_AML_WATCHDOG)
 static struct platform_device aml_wdt_device = {
@@ -1973,6 +2015,7 @@ static struct platform_device __initdata *platform_devs[] = {
 	&gx1001_device,
 	&avl6211_device,
 	&ite9173_device,
+	&ite9133_device,
 #endif
  #if defined(CONFIG_AML_WATCHDOG)
         &aml_wdt_device,
@@ -2101,6 +2144,18 @@ static void __init device_pinmux_init(void )
 //	clear_mio_mux(0, 1<<4);
 	clear_mio_mux(0, 0x7);
 #endif
+
+#ifdef CONFIG_AM_ITE9133
+
+//for ite9133
+	printk("CONFIG_AM_ITE9133 set pinmux\n");
+	set_mio_mux(3, 0xFFF<<6);
+//	clear_mio_mux(0, 1<<4);
+	clear_mio_mux(0, 0x3F);
+
+
+#endif
+
 
 
 #if defined(CONFIG_SDIO_DHD_CDC_WIFI_40181_MODULE_MODULE)
