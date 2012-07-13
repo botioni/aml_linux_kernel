794c434416affd3a8d1aca41193eebde73d97192
jian.xu <jian.xu@amlogic.com>
2012-07-13 13:32:55 +0800

    add sys for dd+ drc control

--- drivers/amlogic/audiodsp/audiodsp_module.c
+++ drivers/amlogic/audiodsp/audiodsp_module.c
@@ -48,6 +48,19 @@ extern unsigned IEC958_mode_codec;
 extern int decopt;
 static int IEC958_mode_raw_last = 0;
 static int IEC958_mode_codec_last = 0;
+/* code for DD/DD+ DRC control  */
+/* Dynamic range compression mode */
+typedef enum {
+    GBL_COMP_CUSTOM_0 = 0, /* custom mode, analog  dialnorm */
+    GBL_COMP_CUSTOM_1,     /* custom mode, digital dialnorm */
+    GBL_COMP_LINE,         /* line out mode (default)       */
+    GBL_COMP_RF            /* RF mode                       */
+} DDP_DEC_DRC_MODE;
+#define DRC_MODE_BIT  0
+#define DRC_HIGH_CUT_BIT 3
+#define DRC_LOW_BST_BIT 16
+static unsigned ac3_drc_control = (GBL_COMP_LINE<<DRC_MODE_BIT)|(100<<DRC_HIGH_CUT_BIT)|(100<<DRC_LOW_BST_BIT);
+	
 extern struct audio_info * get_audio_info(void);
 extern void	aml_alsa_hw_reprepare();
 void audiodsp_moniter(unsigned long);
@@ -179,6 +192,8 @@ static int audiodsp_ioctl(struct inode *node, struct file *file, unsigned int cm
 			}	
 			else
 				IEC958_mode_codec = 0;
+			if(args == MCODEC_FMT_AC3) //for dd+ certification
+				DSP_WD(DSP_AC3_DRC_INFO,ac3_drc_control|(1<<31));
 			break;
 		case AUDIODSP_START:
 			if(IEC958_mode_codec || (IEC958_mode_codec_last != IEC958_mode_codec))
@@ -802,7 +817,50 @@ static ssize_t dec_option_store(struct class* class, struct class_attribute* att
   printk("dec option=%d\n", dec_opt);
   return count;
 }
+static ssize_t ac3_drc_control_show(struct class*cla, struct class_attribute* attr, char* buf)
+{
+	char *drcmode[] = {"CUSTOM_0","CUSTOM_1","LINE","RF"};
+	char *pbuf = buf;
+	pbuf += sprintf(pbuf, "\tdd+ drc mode : %s\n", drcmode[ac3_drc_control&0x3]);
+	pbuf += sprintf(pbuf, "\tdd+ drc high cut scale : %d%\n", (ac3_drc_control>>DRC_HIGH_CUT_BIT)&0xff);
+	pbuf += sprintf(pbuf, "\tdd+ drc low boost scale : %d%\n", (ac3_drc_control>>DRC_LOW_BST_BIT)&0xff);
+	return (pbuf-buf);
+}
+static ssize_t ac3_drc_control_store(struct class* class, struct class_attribute* attr,
+   const char* buf, size_t count )
+{
+    char tmpbuf[128];
+	char *drcmode[] = {"CUSTOM_0","CUSTOM_1","LINE","RF"};	
+    int i=0;
+	unsigned val;
+    while((buf[i])&&(buf[i]!=',')&&(buf[i]!=' ')){
+        tmpbuf[i]=buf[i];
+        i++;    
+    }
+    tmpbuf[i]=0;
+ 	if(strncmp(tmpbuf, "drcmode", 7)==0){
+        val=simple_strtoul(buf+i+1, NULL, 16); 
+		val = val&0x3;
+		printk("drc mode set to %s\n",drcmode[val]);
+		ac3_drc_control = (ac3_drc_control&(~3))|val;
+ 	}
+ 	else if(strncmp(tmpbuf, "drchighcutscale", 15)==0){
+        val=simple_strtoul(buf+i+1, NULL, 16); 	
+		val = val&0xff;
+		printk("drc high cut scale set to %d%\n",val);
+		ac3_drc_control = (ac3_drc_control&(~(0xff<<DRC_HIGH_CUT_BIT)))|(val<<DRC_HIGH_CUT_BIT);
+ 	}
+ 	else if(strncmp(tmpbuf, "drclowboostscale", 16)==0){
+        val=simple_strtoul(buf+i+1, NULL, 16); 	
+		val = val&0xff;
+		printk("drc low boost scale set to %d%\n",val);
+		ac3_drc_control = (ac3_drc_control&(~(0xff<<DRC_LOW_BST_BIT)))|(val<<DRC_LOW_BST_BIT);
+ 	}
+	else
+		printk("invalid args\n");
+	return count;
 
+}
 
 static struct class_attribute audiodsp_attrs[]={
     __ATTR_RO(codec_fmt),
@@ -812,7 +870,8 @@ static struct class_attribute audiodsp_attrs[]={
     __ATTR_RO(dsp_working_status),
     __ATTR(digital_raw, S_IRUGO | S_IWUSR, digital_raw_show, digital_raw_store),
 	__ATTR(dec_option, S_IRUGO | S_IWUSR, dec_option_show, dec_option_store),    
-	__ATTR(print_flag, S_IRUGO | S_IWUSR, print_flag_show, print_flag_store),	 
+	__ATTR(print_flag, S_IRUGO | S_IWUSR, print_flag_show, print_flag_store),	
+	__ATTR(ac3_drc_control, S_IRUGO | S_IWUSR, ac3_drc_control_show, ac3_drc_control_store),
     __ATTR_NULL
 };
 
