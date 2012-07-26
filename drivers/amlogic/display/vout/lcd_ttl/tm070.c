193940ab5e0cb1b99d1dcb178c99091473ba9c62
Geng Li <geng.li@amlogic.com>
2012-07-26 19:15:19 +0800

    Update compile error of TM070 driver

--- drivers/amlogic/display/vout/lcd_ttl/tm070.c
+++ drivers/amlogic/display/vout/lcd_ttl/tm070.c
@@ -308,6 +308,13 @@ static void t13_power_off(void)
     	power_off_lcd();
 }
 
+void shut_down_lcd(void)
+{
+    printk("shut down lcd...\n");
+    power_off_backlight();
+    power_off_lcd();
+}
+
 static void t13_io_init(void)
 {
     PRINT_INFO("\n\nT13 LCD Init.\n\n");
