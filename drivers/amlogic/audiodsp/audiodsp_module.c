ee55a1e08e9a99aa204c76dd2f9cd96c4ded0602
jian.xu <jian.xu@amlogic.com>
2012-08-09 16:03:19 +0800

    fix eac3/ac3 type use the same firmware problem

--- drivers/amlogic/audiodsp/audiodsp_module.c
+++ drivers/amlogic/audiodsp/audiodsp_module.c
@@ -126,8 +126,8 @@ int audiodsp_start(void)
 	   (pmcode->fmt == MCODEC_FMT_PCM)  ||
 	   (pmcode->fmt == MCODEC_FMT_WMAPRO)||
 	   (pmcode->fmt == MCODEC_FMT_ALAC)||
-	   (pmcode->fmt == MCODEC_FMT_AC3) ||
-	   (pmcode->fmt == MCODEC_FMT_EAC3) ||	  
+	   (pmcode->fmt & MCODEC_FMT_AC3) ||
+	   (pmcode->fmt & MCODEC_FMT_EAC3) ||	  
 	  (pmcode->fmt == MCODEC_FMT_APE) ||
 	  (pmcode->fmt == MCODEC_FMT_FLAC))
 
