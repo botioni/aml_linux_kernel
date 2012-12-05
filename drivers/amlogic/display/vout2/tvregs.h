d6f1fa8a900e7b71f52da8fa9a4d48b363f2c0c2
jets.yan <jets.yan@amlogic.com>
2012-12-05 17:44:59 +0800

    Improve video performance of cvbs in dual-output-mode according to single cvbs output mode

--- drivers/amlogic/display/vout2/tvregs.h
+++ drivers/amlogic/display/vout2/tvregs.h
@@ -279,7 +279,7 @@ static const reg_t tvregs_480cvbs[] = {
     {ENCI_VIDEO_SCH,             0x20,  },
     {ENCI_SYNC_MODE,             0x07,  },
     {ENCI_MACV_N0,               0,     },
-    {ENCI_YC_DELAY,                0x371, },
+    {ENCI_YC_DELAY,                0x345, },
     {ENCI_VFIFO2VD_PIXEL_START,    0xf3,  },
     {ENCI_VFIFO2VD_PIXEL_END,      0x0693,},
     {ENCI_VFIFO2VD_LINE_TOP_START, 0x12,  },
@@ -316,6 +316,11 @@ static const reg_t tvregs_480cvbs[] = {
     {ENCI_DACSEL_1,                   0x11       },
 #endif    
     {ENCI_VIDEO_EN,              1,     },
+    {VENC_VDAC_DAC3_GAINCTRL,	 0x30	},
+    {ENCI_VIDEO_BRIGHT,		 	 0x2	},
+    {ENCI_VIDEO_SAT,		 	 0xff	},
+    {VENC_VDAC_DAC3_FILT_CTRL0,	 0x1	},
+    {VENC_VDAC_DAC3_FILT_CTRL1,	 0xf850	},
     {MREG_END_MARKER,            0      }
 };
 
@@ -473,7 +478,7 @@ static const reg_t tvregs_576cvbs[] = {
     {ENCI_VIDEO_SCH,             0x28,  },
     {ENCI_SYNC_MODE,             0x07,  },
     {ENCI_MACV_N0,               0,     },
-    {ENCI_YC_DELAY,              0x371, },
+    {ENCI_YC_DELAY,              0x333, },
     {ENCI_VFIFO2VD_PIXEL_START,       0x010b     },
     {ENCI_VFIFO2VD_PIXEL_END,         0x06ab     },
     {ENCI_VFIFO2VD_LINE_TOP_START,    0x0016     },
@@ -510,6 +515,10 @@ static const reg_t tvregs_576cvbs[] = {
     {ENCI_DACSEL_1,                   0x11       },
 #endif    
     {ENCI_VIDEO_EN,              1,     },
+    {VENC_VDAC_DAC3_GAINCTRL,	 0x23	},
+    {ENCI_VIDEO_SAT,		 0x8	},
+    {VENC_VDAC_DAC3_FILT_CTRL0,	 0x1	},
+    {VENC_VDAC_DAC3_FILT_CTRL1,	 0xfc48	},
     {MREG_END_MARKER,            0      }
 };
 
