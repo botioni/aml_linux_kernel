d0ee3309ce353d53ed1329ea8550cd663496a25c
zhiqiang.han <zhiqiang.han@amlogic.com>
2012-12-19 14:05:38 +0800

    [DVB] smartcard update, by Live.Li

--- drivers/amlogic/smartcard/smartcard.c
+++ drivers/amlogic/smartcard/smartcard.c
@@ -115,6 +115,35 @@ static struct class smc_class = {
     .class_attrs = smc_class_attrs,
 };
 
+typedef enum sc_type {
+	SC_DIRECT,
+	SC_INVERSE,
+} sc_type_t;
+static sc_type_t smcTpye = SC_DIRECT;
+static int f_table[16] = {
+	372, 372, 558, 744, 1116, 1488, 1860, -1, -1, 512, 768, 1024, 1536, 2048, -1, -1
+};
+static int d_table[16] = {
+	-1, 1, 2, 4, 8, 16, 32, 64, -1, 20, -1, -1, -1, -1, -1, -1
+};
+static unsigned char inv_table[256] = {
+	0xFF, 0x7F, 0xBF, 0x3F, 0xDF, 0x5F, 0x9F, 0x1F, 0xEF, 0x6F, 0xAF, 0x2F, 0xCF, 0x4F, 0x8F, 0x0F,
+	0xF7, 0x77, 0xB7, 0x37, 0xD7, 0x57, 0x97, 0x17, 0xE7, 0x67, 0xA7, 0x27, 0xC7, 0x47, 0x87, 0x07,
+	0xFB, 0x7B, 0xBB, 0x3B, 0xDB, 0x5B, 0x9B, 0x1B, 0xEB, 0x6B, 0xAB, 0x2B, 0xCB, 0x4B, 0x8B, 0x0B,
+	0xF3, 0x73, 0xB3, 0x33, 0xD3, 0x53, 0x93, 0x13, 0xE3, 0x63, 0xA3, 0x23, 0xC3, 0x43, 0x83, 0x03,
+	0xFD, 0x7D, 0xBD, 0x3D, 0xDD, 0x5D, 0x9D, 0x1D, 0xED, 0x6D, 0xAD, 0x2D, 0xCD, 0x4D, 0x8D, 0x0D,
+	0xF5, 0x75, 0xB5, 0x35, 0xD5, 0x55, 0x95, 0x15, 0xE5, 0x65, 0xA5, 0x25, 0xC5, 0x45, 0x85, 0x05,
+	0xF9, 0x79, 0xB9, 0x39, 0xD9, 0x59, 0x99, 0x19, 0xE9, 0x69, 0xA9, 0x29, 0xC9, 0x49, 0x89, 0x09,
+	0xF1, 0x71, 0xB1, 0x31, 0xD1, 0x51, 0x91, 0x11, 0xE1, 0x61, 0xA1, 0x21, 0xC1, 0x41, 0x81, 0x01,
+	0xFE, 0x7E, 0xBE, 0x3E, 0xDE, 0x5E, 0x9E, 0x1E, 0xEE, 0x6E, 0xAE, 0x2E, 0xCE, 0x4E, 0x8E, 0x0E,
+	0xF6, 0x76, 0xB6, 0x36, 0xD6, 0x56, 0x96, 0x16, 0xE6, 0x66, 0xA6, 0x26, 0xC6, 0x46, 0x86, 0x06,
+	0xFA, 0x7A, 0xBA, 0x3A, 0xDA, 0x5A, 0x9A, 0x1A, 0xEA, 0x6A, 0xAA, 0x2A, 0xCA, 0x4A, 0x8A, 0x0A,
+	0xF2, 0x72, 0xB2, 0x32, 0xD2, 0x52, 0x92, 0x12, 0xE2, 0x62, 0xA2, 0x22, 0xC2, 0x42, 0x82, 0x02,
+	0xFC, 0x7C, 0xBC, 0x3C, 0xDC, 0x5C, 0x9C, 0x1C, 0xEC, 0x6C, 0xAC, 0x2C, 0xCC, 0x4C, 0x8C, 0x0C,
+	0xF4, 0x74, 0xB4, 0x34, 0xD4, 0x54, 0x94, 0x14, 0xE4, 0x64, 0xA4, 0x24, 0xC4, 0x44, 0x84, 0x04,
+	0xF8, 0x78, 0xB8, 0x38, 0xD8, 0x58, 0x98, 0x18, 0xE8, 0x68, 0xA8, 0x28, 0xC8, 0x48, 0x88, 0x08,
+	0xF0, 0x70, 0xB0, 0x30, 0xD0, 0x50, 0x90, 0x10, 0xE0, 0x60, 0xA0, 0x20, 0xC0, 0x40, 0x80, 0x00
+};
 #ifdef CONFIG_ARCH_ARC700
 extern unsigned long get_mpeg_clk(void);
 #else
@@ -315,7 +344,7 @@ static int smc_hw_get(smc_dev_t *smc, int cnt, int timeout)
 	unsigned long sc_status;
 	int times = timeout*100;
 	SMC_STATUS_Reg_t *sc_status_reg = (SMC_STATUS_Reg_t*)&sc_status;
-	
+	printk("atr");
 	while((times > 0) && (cnt > 0)) {
 		sc_status = SMC_READ_REG(STATUS);
 						
@@ -338,10 +367,18 @@ static int smc_hw_get(smc_dev_t *smc, int cnt, int timeout)
 		{
 			while(sc_status_reg->recv_fifo_bytes_number > 0)
 			{
-				smc->atr.atr[smc->atr.atr_len++] = (SMC_READ_REG(FIFO))&0xff;
+				unsigned char byte = 0xff;
+				if(SC_DIRECT == smcTpye)
+					byte= (SMC_READ_REG(FIFO))&0xff;
+				else
+					byte = inv_table[(SMC_READ_REG(FIFO))&0xff];
+					printk("%x \n",byte);
+					smc->atr.atr[smc->atr.atr_len++] = byte;
 				sc_status_reg->recv_fifo_bytes_number--;
 				cnt--;
-				if(cnt==0) return 0;
+				if(cnt==0) {
+					return 0;
+				}
 			}
 		}
 	}
@@ -354,10 +391,30 @@ static int smc_hw_read_atr(smc_dev_t *smc)
 	char *ptr = smc->atr.atr;
 	int his_len, t, tnext = 0, only_t0 = 1, loop_cnt=0;
 	
+	unsigned char TS = 0, TA1 = 0xFF, TC1 = 0xFF;
 	pr_dbg("read atr\n");
 	
 	smc->atr.atr_len = 0;
-	if(smc_hw_get(smc, 2, 2000)<0)
+	if(smc_hw_get(smc, 1, 2000)<0)
+		goto end;
+
+	TS = ptr[0];
+	printk("TS = %x\n",TS);
+	if (0x03 == TS ) { // Inverse convention
+		ptr[0] = inv_table[ptr[0]];
+		smcTpye = SC_INVERSE;
+	} else if ( 0x3F == TS) {  // Inverse convention,but TS needn't invert.
+		smcTpye = SC_INVERSE;
+	} else if ( 0x3B == TS) {  // Direct convention
+		smcTpye = SC_DIRECT;
+	} else if ( 0x23 == TS) {  // Direct convention,but TS need invert.
+		ptr[0] = inv_table[ptr[0]];
+		smcTpye = SC_DIRECT;
+	} else {
+		goto end;
+	}
+
+	if(smc_hw_get(smc, 1, 2000)<0)
 		goto end;
 	
 	ptr++;
@@ -522,12 +579,20 @@ static int smc_hw_start_send(smc_dev_t *smc)
 		}
 			
 		byte = smc->send_buf[smc->send_start++];
-		SMC_WRITE_REG(FIFO, byte);
+		if(SC_DIRECT == smcTpye)
+			SMC_WRITE_REG(FIFO, byte);
+		else
+			SMC_WRITE_REG(FIFO, inv_table[byte]);
 		smc->send_start %= SEND_BUF_SIZE;
 		smc->send_count--;
 		cnt++;
 	}
 	
+    if(!smc->send_count) {
+        sc_int_reg->send_fifo_last_byte_int_mask = 0;
+        sc_int_reg->recv_fifo_bytes_threshold_int_mask = 1;
+    }
+
 	spin_unlock_irqrestore(&smc->slock, flags);
 	
 	pr_dbg("send %d bytes to hw\n", cnt);
@@ -546,18 +611,22 @@ static irqreturn_t smc_irq_handler(int irq, void *data)
 	SMCCARD_HW_Reg0_t *sc_reg0_reg = (void *)&sc_reg0;
 
 	sc_int = SMC_READ_REG(INTR);
+	sc_status = SMC_READ_REG(STATUS);
 	
 	if(sc_int_reg->recv_fifo_bytes_threshold_int) {
 		if(smc->recv_count==RECV_BUF_SIZE) {
 			pr_error("receive buffer overflow\n");
 		} else {
 			int pos = smc->recv_start+smc->recv_count;
-			
-			pos %= RECV_BUF_SIZE;
-			smc->recv_buf[pos] = SMC_READ_REG(FIFO);
-			smc->recv_count++;
-			
-			pr_dbg("irq: recv 1 byte\n");
+			while(sc_status_reg->recv_fifo_bytes_number) {			
+				pos %= RECV_BUF_SIZE;
+				if(SC_DIRECT == smcTpye)
+					smc->recv_buf[pos++] = SMC_READ_REG(FIFO)&0xff;
+				else
+					smc->recv_buf[pos++] = inv_table[SMC_READ_REG(FIFO)&0xff];
+				smc->recv_count++;
+				sc_status = SMC_READ_REG(STATUS);
+			}	
 		}
 		
 		sc_int_reg->recv_fifo_bytes_threshold_int = 0;
@@ -577,13 +646,17 @@ static irqreturn_t smc_irq_handler(int irq, void *data)
 			}
 			
 			byte = smc->send_buf[smc->send_start++];
-			SMC_WRITE_REG(FIFO, byte);
+			if(SC_DIRECT == smcTpye)
+				SMC_WRITE_REG(FIFO, byte);
+			else
+				SMC_WRITE_REG(FIFO, inv_table[byte]);
 			smc->send_start %= SEND_BUF_SIZE;
 			smc->send_count--;
 			cnt++;
 		}
 		
 		pr_dbg("irq: send %d bytes to hw\n", cnt);
+		printk("irq: send %d bytes to hw\n", cnt);
 		
 		if(!smc->send_count) {
 			sc_int_reg->send_fifo_last_byte_int_mask = 0;
